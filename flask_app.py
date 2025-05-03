a6e8580178bb6376a0390daa156ea5f543487296
CWeichsel <c.weichsel@gmx.de>
2025-05-03 12:41:26 +0200

    ad csv

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,23 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
+with open('data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+
 @app.route('/')
 def hello_world():
-    return 'Hello from cweichsel 2!'
+    with open('data.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        rows = []
+        for row in spamreader:
+            rows.append(', '.join(row))
+    return render_template("index.html", test = rows)
+
+if __name__ == "__main__":
+    app.run(debug=True)
